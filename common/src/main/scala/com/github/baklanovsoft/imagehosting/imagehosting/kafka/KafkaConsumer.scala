a0da1a9ac5a73cddad7e2a8e784cbc510a564a66
DenisNovac <ponydnk@gmail.com>
2024-02-04 02:10:06 +0400

    IHP-4 - add kafka consumer with deserializer

---
+++ common/src/main/scala/com/github/baklanovsoft/imagehosting/imagehosting/kafka/KafkaConsumer.scala
@@ -0,0 +1,23 @@
+package com.github.baklanovsoft.imagehosting.imagehosting.kafka
+
+import cats.effect.kernel.{Async, Resource}
+import fs2.kafka._
+import fs2.Stream
+
+class KafkaConsumer[F[_]: Async, K, V](servers: String, consumerGroup: String, topic: String)(implicit
+    keyDeserializer: Resource[F, KeyDeserializer[F, K]],
+    valueDeserializer: Resource[F, ValueDeserializer[F, V]]
+) {
+
+  private val consumerSettings =
+    ConsumerSettings[F, K, V]
+      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers(servers)
+      .withGroupId(consumerGroup)
+
+  def stream: Stream[F, CommittableConsumerRecord[F, K, V]] =
+    KafkaConsumer
+      .stream(consumerSettings)
+      .subscribeTo(topic)
+      .records
+}
---
+++ common/src/main/scala/com/github/baklanovsoft/imagehosting/imagehosting/kafka/KafkaJsonDeserializer.scala
@@ -0,0 +1,30 @@
+package com.github.baklanovsoft.imagehosting.imagehosting.kafka
+
+import cats.effect.kernel.Sync
+import cats.implicits._
+import com.github.baklanovsoft.imagehosting.error.DecodingError
+import com.github.baklanovsoft.imagehosting.imagehosting.kafka.KafkaJsonDeserializer.KafkaJsonDecodingError
+import fs2.kafka._
+import io.circe.{Decoder, parser}
+
+trait KafkaJsonDeserializer {
+
+  implicit def deserializer[F[_]: Sync, T: Decoder]: Deserializer[F, T] =
+    Deserializer.instance { (_, _, bytes) =>
+      Sync[F].delay(new String(bytes)).flatMap { str =>
+        parser
+          .parse(str)
+          .flatMap(Decoder[T].decodeJson) match {
+          case Left(value)  =>
+            KafkaJsonDecodingError(s"Can't decode json kafka message: ${value.getMessage}. Original message: $str")
+              .raiseError[F, T]
+          case Right(value) => value.pure[F]
+        }
+      }
+    }
+
+}
+
+object KafkaJsonDeserializer {
+  final case class KafkaJsonDecodingError(message: String) extends DecodingError
+}
