b9f264be7d797e22a72108f1867f44180bfe9f91
DenisNovac <ponydnk@gmail.com>
2024-02-04 12:48:47 +0400

    IHP-4 - make circe decode bytes directly

--- common/src/main/scala/com/github/baklanovsoft/imagehosting/imagehosting/kafka/KafkaJsonDeserializer.scala
+++ common/src/main/scala/com/github/baklanovsoft/imagehosting/imagehosting/kafka/KafkaJsonDeserializer.scala
@@ -5,21 +5,20 @@ import cats.implicits._
 import com.github.baklanovsoft.imagehosting.error.DecodingError
 import com.github.baklanovsoft.imagehosting.imagehosting.kafka.KafkaJsonDeserializer.KafkaJsonDecodingError
 import fs2.kafka._
-import io.circe.{Decoder, parser}
+import io.circe.Decoder
+import io.circe.jawn.decodeByteArray
 
 trait KafkaJsonDeserializer {
 
   implicit def deserializer[F[_]: Sync, T: Decoder]: Deserializer[F, T] =
     Deserializer.instance { (_, _, bytes) =>
-      Sync[F].delay(new String(bytes)).flatMap { str =>
-        parser
-          .parse(str)
-          .flatMap(Decoder[T].decodeJson) match {
-          case Left(value)  =>
-            KafkaJsonDecodingError(s"Can't decode json kafka message: ${value.getMessage}. Original message: $str")
-              .raiseError[F, T]
-          case Right(value) => value.pure[F]
-        }
+      decodeByteArray[T](bytes) match {
+        case Left(value)  =>
+          KafkaJsonDecodingError(
+            s"Can't decode json kafka message: ${value.getMessage}. Original message: ${new String(bytes)}"
+          )
+            .raiseError[F, T]
+        case Right(value) => value.pure[F]
       }
     }
 
