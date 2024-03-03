f56c4c8258b48f310f5187885b4555dd0bef423a
Denis Novac <ponydnk@gmail.com>
2024-03-03 11:52:26 +0400

    [IHP-22] Readme with messages protocol (#25)

--- recognizer/src/main/scala/com/github/baklanovsoft/imagehosting/recognizer/AppConfig.scala
+++ recognizer/src/main/scala/com/github/baklanovsoft/imagehosting/recognizer/AppConfig.scala
@@ -9,7 +9,7 @@ final case class AppConfig(
     newImagesTopic: String,
     categoriesTopic: String,
     debugCategories: Boolean,
-    nsfwSynsetPath: String,
+    nsfwSynset: String,
     nsfwModelPath: String,
     minio: MinioCreds
 )
